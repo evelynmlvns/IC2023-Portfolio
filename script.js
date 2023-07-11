0e6846d382c8009509bc83c8f5a7de72d59769e6
evelynmlvns <137207921+evelynmlvns@users.noreply.github.com>
2023-07-11 12:16:55 -0400

    final touches

--- script.js
+++ script.js
@@ -1,42 +1,147 @@
-let abtMeButton= document.querySelector(".abtMeButton")
-let accomplishmentsButton= document.querySelector(".accomplishmentsButton")
-let hobbiesButton= document.querySelector(".hobbiesButton")
-let plansButton= document.querySelector(".plansButton")
-let projectsButton= document.querySelector(".projectsButton")
-let contactButton= document.querySelector(".contact")
-let homePage= document.querySelector(".homePage")
-let abtMePage= document.querySelector(".abtMePage")
-let accomplishmentsPage= document.querySelector(".accomplishmentsPage")
-let hobbiesPage= document.querySelector(".hobbiesPage")
-let careersPage= document.querySelector(".careersPage")
-let projectsPage= document.querySelector(".projectsPage")
+//HomePage button functions when clicked (first Page) //
 
+function aboutPageClick() {
+    console.log("check");
+    let abtMePage= document.querySelector(".abtMePage")
+    let homePage= document.querySelector(".homePage")
 
-
-//HomePage button functions when clicked //
-abtMeButton.onclick=function(){
     homePage.style.display="none";
     abtMePage.style.display="block";
 }
 
-accomplishmentsButton.onclick= function(){
+function accomplishmentsClick() {
+    let homePage= document.querySelector(".homePage")
+    let accomplishmentsPage= document.querySelector(".accomplishmentsPage")
+
     homePage.style.display="none";
     accomplishmentsPage.style.display="block";
 }
-hobbiesButton.onclick= function(){
+
+function hobbiesClick() {
+    let homePage= document.querySelector(".homePage")
+    let hobbiesPage= document.querySelector(".hobbiesPage")
+
     homePage.style.display="none";
     hobbiesPage.style.display= "block";
 }
-careersButton.onclick= function(){
+
+function careersClick() {
+    let homePage= document.querySelector(".homePage")
+    let careersPage= document.querySelector(".careersPage")
+
     homePage.style.display="none";
     careersPage.style.display= "block";
 }
-projectsButton.onclick= function(){
+
+function projectsClick() {
+    let homePage= document.querySelector(".homePage")
+    let projectsPage= document.querySelector(".projectsPage")
+
     homePage.style.display="none";
     projectsPage.style.display= "block";
 }
 
-contactButton.onclick= function(){
-    homePage.style.display="none";
+// function contactClick(){
 
+//     homePage.style.display="none";
+// }
+
+
+
+
+//navigation bars function so they arent blocked//
+
+function homeBarClick() {
+    let homePage= document.querySelector(".homePage")
+    let abtMePage= document.querySelector(".abtMePage")
+    let accomplishmentsPage= document.querySelector(".accomplishmentsPage")
+    let hobbiesPage= document.querySelector(".hobbiesPage")
+    let careersPage= document.querySelector(".careersPage")
+    let projectsPage= document.querySelector(".projectsPage")
+
+    homePage.style.display="block"
+    abtMePage.style.display="none"
+    accomplishmentsPage.style.display="none"
+    hobbiesPage.style.display="none"
+    careersPage.style.display="none"
+    projectsPage.style.display="none"
+}
+
+function accomplishBarClick() {
+    let homePage= document.querySelector(".homePage")
+    let abtMePage= document.querySelector(".abtMePage")
+    let accomplishmentsPage= document.querySelector(".accomplishmentsPage")
+    let hobbiesPage= document.querySelector(".hobbiesPage")
+    let careersPage= document.querySelector(".careersPage")
+    let projectsPage= document.querySelector(".projectsPage")
+
+    homePage.style.display="none"
+    abtMePage.style.display="none"
+    accomplishmentsPage.style.display="block"
+    hobbiesPage.style.display="none"
+    careersPage.style.display="none"
+    projectsPage.style.display="none"
+}
+
+function abtMeBarClick() {
+    let homePage= document.querySelector(".homePage")
+    let abtMePage= document.querySelector(".abtMePage")
+    let accomplishmentsPage= document.querySelector(".accomplishmentsPage")
+    let hobbiesPage= document.querySelector(".hobbiesPage")
+    let careersPage= document.querySelector(".careersPage")
+    let projectsPage= document.querySelector(".projectsPage")
+
+    homePage.style.display="none"
+    abtMePage.style.display="block"
+    accomplishmentsPage.style.display="none"
+    hobbiesPage.style.display="none"
+    careersPage.style.display="none"
+    projectsPage.style.display="none"
+}
+function hobbiesBarClick() {
+    let homePage= document.querySelector(".homePage")
+    let abtMePage= document.querySelector(".abtMePage")
+    let accomplishmentsPage= document.querySelector(".accomplishmentsPage")
+    let hobbiesPage= document.querySelector(".hobbiesPage")
+    let careersPage= document.querySelector(".careersPage")
+    let projectsPage= document.querySelector(".projectsPage")
+
+    homePage.style.display="none"
+    abtMePage.style.display="none"
+    accomplishmentsPage.style.display="none"
+    hobbiesPage.style.display="block"
+    careersPage.style.display="none"
+    projectsPage.style.display="none"
+}
+
+function careersBarClick() {
+    let homePage= document.querySelector(".homePage")
+    let abtMePage= document.querySelector(".abtMePage")
+    let accomplishmentsPage= document.querySelector(".accomplishmentsPage")
+    let hobbiesPage= document.querySelector(".hobbiesPage")
+    let careersPage= document.querySelector(".careersPage")
+    let projectsPage= document.querySelector(".projectsPage")
+
+    homePage.style.display="none"
+    abtMePage.style.display="none"
+    accomplishmentsPage.style.display="none"
+    hobbiesPage.style.display="none"
+    careersPage.style.display="block"
+    projectsPage.style.display="none"
+}
+
+function projectsBarClick() {
+    let homePage= document.querySelector(".homePage")
+    let abtMePage= document.querySelector(".abtMePage")
+    let accomplishmentsPage= document.querySelector(".accomplishmentsPage")
+    let hobbiesPage= document.querySelector(".hobbiesPage")
+    let careersPage= document.querySelector(".careersPage")
+    let projectsPage= document.querySelector(".projectsPage")
+
+    homePage.style.display="none"
+    abtMePage.style.display="none"
+    accomplishmentsPage.style.display="none"
+    hobbiesPage.style.display="none"
+    careersPage.style.display="none"
+    projectsPage.style.display="block"
 }
